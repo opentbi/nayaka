3854a24522eaf3bd57e4d1028fbb323304ccf926
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2022-12-27 10:51:15 +0000

    feat: completed config types
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

--- src/types/config.ts
+++ src/types/config.ts
@@ -4,6 +4,29 @@
  * Nayaka merupakan Open Source Software dengan Apache License V2.
  * Anda dapat mengedit atau mendistribusikan ulang sesuai dengan syarat dan ketentuan dari Apache License.
  */
+
+interface Feature {
+	prohibitedLinks: {
+		enabled: boolean;
+		sendToGroup: boolean;
+		action: 'delete' | 'ignore' | 'kick';
+		links: string[];
+	};
+	prohibitedWords: Omit<Feature['prohibitedLinks'], 'links'> & {
+		words: string[];
+	};
+	noGroupPromotions:
+		& Omit<Feature['prohibitedLinks'], 'links' | 'sendToGroup'>
+		& {
+			tags: boolean;
+			links: boolean;
+			messageEnabled: boolean;
+			bypassHashtags: string[];
+		};
+}
 export interface IConfig {
 	developers: string[];
+	customMessages: Record<string, string>;
+	features: Feature;
+	appliedGroupsConfig: string[];
 }
