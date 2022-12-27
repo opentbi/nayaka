94c11f67922b3b9b73b6c0205b51c538aaecb5c7
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2022-12-27 15:14:46 +0000

    deps: add gql-query-builder
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -11,3 +11,4 @@ export * as GrammyTypes from 'https://deno.land/x/grammy@v1.12.0/types.deno.ts';
 export { serve as serveHttp } from 'https://deno.land/std@0.167.0/http/server.ts';
 export * as path from 'https://deno.land/std@0.102.0/path/mod.ts';
 export * as YAML from 'https://deno.land/std@0.167.0/encoding/yaml.ts';
+export * as GQLQueryBuilder from 'https://ga.jspm.io/npm:gql-query-builder@3.8.0/build/index.js';
